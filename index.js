026d7a2911abd4a0dd189c12cc477dcb6afb9bb3
akjilani120 <abdulkaderjilani120@gmail.com>
2022-06-02 23:26:40 +0600

    added mongodb

--- index.js
+++ index.js
@@ -2,9 +2,20 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000
 const cors = require('cors');
-
+require('dotenv').config()
+const { MongoClient, ServerApiVersion } = require('mongodb');
  app.use(cors())
  app.use(express.json())
+ 
+
+const uri = "mongodb+srv://akjilani:<password>@cluster0.5pf8i.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
 app.get('/', (req, res) => {
   res.send('car part shop home router')
 })
