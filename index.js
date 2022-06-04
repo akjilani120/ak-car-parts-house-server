817b3f850c0fb9a3b89717715db6d7f9dbac47da
akjilani120 <abdulkaderjilani120@gmail.com>
2022-06-04 23:58:07 +0600

    special car service data added

--- index.js
+++ index.js
@@ -8,14 +8,29 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
  app.use(express.json())
  
 
-const uri = "mongodb+srv://akjilani:<password>@cluster0.5pf8i.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.5pf8i.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
 
+async function run(){
+  
+try{
+  await client.connect();
+  const populerCarcollection = client.db("carHouse").collection("populerCar");
+  const specialCarcollection = client.db("carHouse").collection("specialCar");
+  app.get("/populerCar" , async(req , res) =>{
+    const result = await populerCarcollection.find().toArray()
+    res.send(result)
+  })
+  app.get("/specialCar" , async(req , res) =>{
+    const result = await specialCarcollection.find().toArray()
+    res.send(result)
+  })
+ 
+}finally{
+
+}
+}
+run().catch(console.dir);
 app.get('/', (req, res) => {
   res.send('car part shop home router')
 })
