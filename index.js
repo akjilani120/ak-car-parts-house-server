842cd484311aca4a82afabba0c593aaddb673ef0
akjilani120 <abdulkaderjilani120@gmail.com>
2022-06-26 02:16:07 +0600

    added some data

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = process.env.PORT || 5000
 const cors = require('cors');
 require('dotenv').config()
-const { MongoClient, ServerApiVersion, ObjectId , ObjectID  } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId  } = require('mongodb');
 const jwt = require('jsonwebtoken');
  app.use(cors())
  app.use(express.json())
@@ -11,6 +11,10 @@ const jwt = require('jsonwebtoken');
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.5pf8i.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+ 
+function verifyJWT (req , res , next){
+
+}
 
 async function run(){
   
@@ -42,8 +46,20 @@ try{
     const result = await offerCarcollection.find().toArray()
     res.send(result)
   })
+  app.get('/offerCar/:id' , async (req , res) =>{
+    const id = req.params.id;   
+    const query = { _id: ObjectId(id) }
+    const result = await offerCarcollection.findOne(query)    
+    res.send(result)
+  })
   app.get("/lamborghini" , async(req , res) =>{
     const result = await lamborghiniCarcollection.find().toArray()
+    res.send(result)
+  })
+  app.get('/lamborghini/:id' , async (req , res) =>{
+    const id = req.params.id;   
+    const query = { _id: ObjectId(id) }
+    const result = await lamborghiniCarcollection.findOne(query)    
     res.send(result)
   })
   app.get("/bugatti" , async(req , res) =>{
@@ -72,6 +88,39 @@ try{
     const order = req.body   
     const result = await orderCarcollection.insertOne(order)
     res.send(result)
+  })
+  app.put("/specialCar/:id" , async(req , res) =>{
+    const id = req.params.id;    
+    const filter={_id: ObjectId(id) }
+    const comment = req.body     
+    const options = { upsert: true };
+    const updateDoc = {
+     $set: comment
+   };
+   const result = await specialCarcollection.updateOne(filter , updateDoc, options )
+   res.send(result)
+  })
+  app.put("/offerCar/:id" , async(req , res) =>{
+    const id = req.params.id;    
+    const filter={_id: ObjectId(id) }
+    const comment = req.body     
+    const options = { upsert: true };
+    const updateDoc = {
+     $set: comment
+   };
+   const result = await offerCarcollection.updateOne(filter , updateDoc, options )
+   res.send(result)
+  })
+  app.put("/lamborghini/:id" , async(req , res) =>{
+    const id = req.params.id;    
+    const filter={_id: ObjectId(id) }
+    const comment = req.body     
+    const options = { upsert: true };
+    const updateDoc = {
+     $set: comment
+   };
+   const result = await lamborghiniCarcollection.updateOne(filter , updateDoc, options )
+   res.send(result)
   })
   
 }finally{
